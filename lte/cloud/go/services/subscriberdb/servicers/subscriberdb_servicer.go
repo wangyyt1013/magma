8816c27fa280830ab39c73f3dfd8e862863cd039
Yuanyuting Wang <yw3241@columbia.edu>
2021-07-22 12:52:40 -0700

    test change 4
    Signed-off-by: Yuanyuting Wang <yw3241@columbia.edu>

--- lte/cloud/go/services/subscriberdb/servicers/subscriberdb_servicer.go
+++ lte/cloud/go/services/subscriberdb/servicers/subscriberdb_servicer.go
@@ -259,7 +259,7 @@ func loadAPNs(gateway *protos.Identity_Gateway) (map[string]*lte_models.ApnConfi
 		return nil, nil, err
 	}
 
-	glog.Infof("---------------I'm a test change3")
+	glog.Infof("---------------I'm a test change4")
 	return apnsByName, apnResources, nil
 }
 
